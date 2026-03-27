df3545953f305699f57ea30ca5b7ef696505c3cb
a01067079208 <a01067079208@gmail.com>
2026-03-27 13:52:45 +0000

    Add dark/light mode toggle feature

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 const numbersContainer = document.getElementById('numbers');
 const generateBtn = document.getElementById('generate');
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
 
 generateBtn.addEventListener('click', () => {
     numbersContainer.innerHTML = '';
